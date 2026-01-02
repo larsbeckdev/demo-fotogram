12f30b9bd2ee2c3da0b8fa4da3fdbed66deca463
larsbeckmusic <larsbeckmusic@gmail.com>
2026-01-02 16:24:43 +0100

    updated router

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,31 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
-})
+  routes: [
+    {
+      path: "/",
+      name: "Home",
+      component: () => import("@/views/home/HomeView.vue"),
+      meta: {
+        title: "Übersicht",
+      },
+    },
+    {
+      path: "/:pathMatch(.*)*",
+      name: "not-found",
+      component: () => import("../views/error/NotFoundView.vue"),
+      meta: {
+        title: "Seite nicht gefunden",
+      },
+    },
+  ],
+});
 
-export default router
+router.afterEach((to) => {
+  document.title = to.meta.title
+    ? `${to.meta.title} – Fotogram`
+    : "Fotogram";
+});
+
+export default router;
